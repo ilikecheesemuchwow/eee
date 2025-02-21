e0f6cdd58818ee71dd69954feb362566b23e2d31
ilikecheesemuchwow <144293882+ilikecheesemuchwow@users.noreply.github.com>
2025-02-20 21:46:29 -0600

    Update duper.lua

--- duper.lua
+++ duper.lua
@@ -1,17 +1,18 @@
 has = getgenv().has 
+has = getgenv().has
 need = getgenv().need
-queue_on_teleport("loadstring(game:HttpGet("https://raw.githubusercontent.com/ilikecheesemuchwow/eee/refs/heads/main/duper.lua",true))()")
-if (has<=need) then
-game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(-42, 341, -281))
-for i,v in pairs(game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):GetChildren()) do
-            if v:IsA("Tool") then
-                v.Parent = game:GetService("Players").LocalPlayer.Character
-                task.wait(0.3)
-                v.Parent = game:GetService("Workspace")
-            end
+queue_on_teleport(getgenv().need = need getgenv().has = (has+1)"loadstring(game:HttpGet(\"https://raw.githubusercontent.com/ilikecheesemuchwow/eee/refs/heads/main/duper.lua",true))()")
+if (has <= need) then
+    game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(-42, 341, -281))
+    for i, v in pairs(game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):GetChildren()) do
+        if v:IsA("Tool") then
+            v.Parent = game:GetService("Players").LocalPlayer.Character
+            task.wait(0.3)
+            v.Parent = game:GetService("Workspace")
         end
-game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
+    end
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
 else
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ilikecheesemuchwow/eee/refs/heads/main/bruh.txt",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ilikecheesemuchwow/eee/refs/heads/main/bruh.txt", true))()
 end
-        
+
