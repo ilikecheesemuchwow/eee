344271ba69cb15cc7457f27cbd8ade60c3e4c44a
ilikecheesemuchwow <144293882+ilikecheesemuchwow@users.noreply.github.com>
2025-02-20 22:03:08 -0600

    Update duper.lua

--- duper.lua
+++ duper.lua
@@ -1,5 +1,4 @@
-queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
-queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ilikecheesemuchwow/eee/refs/heads/main/duper2.lua'))()")
+queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ilikecheesemuchwow/eee/refs/heads/main/duper2.lua'))()")
     game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(-42, 341, -281))
     for i, v in pairs(game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):GetChildren()) do
         if v:IsA("Tool") then
