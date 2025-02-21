5c1985e7f919daa3fd4f5768f633be99535e8ab0
ilikecheesemuchwow <144293882+ilikecheesemuchwow@users.noreply.github.com>
2025-02-20 21:51:58 -0600

    Update duper.lua

--- duper.lua
+++ duper.lua
@@ -1,6 +1,6 @@
 has = getgenv().has
 need = getgenv().need
-queue_on_teleport("getgenv().need = " .. need .. " getgenv().has = " .. (has + 1) .. " loadstring(game:HttpGet('https://raw.githubusercontent.com/ilikecheesemuchwow/eee/refs/heads/main/duper.lua'))()")
+queue_on_teleport(getgenv().need = need getgenv().has = (has+1)"loadstring(game:HttpGet('https://raw.githubusercontent.com/ilikecheesemuchwow/eee/refs/heads/main/duper.lua'))()")
 if (has <= need) then
     game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(-42, 341, -281))
     for i, v in pairs(game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):GetChildren()) do
