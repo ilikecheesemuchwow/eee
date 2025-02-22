2f5425a98995d302a0c8b791974c773a0f934c2f
ilikecheesemuchwow <144293882+ilikecheesemuchwow@users.noreply.github.com>
2025-02-21 18:57:29 -0600

    Update duper2.lua

--- duper2.lua
+++ duper2.lua
@@ -1,11 +1,12 @@
 queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ilikecheesemuchwow/eee/refs/heads/main/duper2.lua'))()")
-    game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(-42, 341, -281))
-    for i, v in pairs(game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):GetChildren()) do
-        if v:IsA("Tool") then
-            v.Parent = game:GetService("Players").LocalPlayer.Character
-            task.wait(0.3)
-            v.Parent = game:GetService("Workspace")
-        end
+game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(-42, 341, -281))
+for i, v in pairs(game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):GetChildren()) do
+    if v:IsA("Tool") then
+        v.Parent = game:GetService("Players").LocalPlayer.Character
+        task.wait(0.3)
+        v.Parent = game:GetService("Workspace")
     end
+end
 task.wait(0.1)
-    game:GetService("TeleportService"):Teleport(game.PlaceId, game.JobId, game:GetService("Players").LocalPlayer)
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, Players.LocalPlayer)
+end
